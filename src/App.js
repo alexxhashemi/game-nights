294e2d1490ae5075b7baf095cb31eb3d373411be
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-17 16:13:39 -0400

    react-router-dom set up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from 'react-router-dom';
 
 function App() {
   const [state, setState] = useState({
     users: [],
     games: {},
   });
- 
+
   axios.get("/api/users").then((response) => {
   });
 
@@ -17,38 +18,51 @@ function App() {
       Promise.all([
         axios.get('/api/users'),
         axios.get('/api/games'),
-        ]).then((all) => {  
-          // console.log('find', all[1].data)
-          setState(prev => ({...prev, users: all[0].data, games: all[1].data}));
-        });
+      ]).then((all) => {
+        // console.log('find', all[1].data)
+        setState(prev => ({ ...prev, users: all[0].data, games: all[1].data }));
       });
-      }, []);
+    });
+  }, []);
 
-        //GAME POST DATA
+  //GAME POST DATA
   let gameObj = {}
   let postVal = Object.values(state.games)
   // console.log('asdas', v)
-  for(let val of postVal) {
+  for (let val of postVal) {
     // console.log('asdasd', d)
-    let parsedVal = val.map(p =>  p)
+    let parsedVal = val.map(p => p)
     gameObj = parsedVal
   }
 
-  
-  let renderPosts = Object.values(gameObj).map( c => {return <article key ={c.id} style={{textAlign: 'center'}}>
-    <h1>{c.title}</h1>
-    {/* <h3>{c.username}</h3> */}
-    <img style={{width: '500px',height: '400px'}} src={c.image} />
-    <h4>Category: {c.category}</h4>
-    <p>{c.description}</p>
+
+  let renderPosts = Object.values(gameObj).map(c => {
+    return <article key={c.id} style={{ textAlign: 'center' }}>
+      <h1>{c.title}</h1>
+      {/* <h3>{c.username}</h3> */}
+      <img style={{ width: '500px', height: '400px' }} src={c.image} />
+      <h4>Category: {c.category}</h4>
+      <p>{c.description}</p>
       <button>JOIN</button>
-      </article>})
+    </article>
+  })
 
   return (
-      <div className="App">
-        {renderPosts}
-      </div>
-    );
+    <div className="App">
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/users" style={{ margin: '10px' }}>Users</Link>
+        <Link to="/games" style={{ margin: '10px' }}>Games</Link>
+        <Link to="/login" style={{ margin: '10px' }}>Login</Link>
+        <Link to="/register" style={{ margin: '10px' }}>Register</Link>
+      </nav>
+      {renderPosts}
+    </div>
+  );
 }
 
 export default App;
