3d91f28de6fffba2c60cf980004a013305755661
ShayFae <ifrahnoor108@gmail.com>
2022-03-20 20:00:43 -0400

    add video

--- src/component/Rooms.js
+++ src/component/Rooms.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RoomItem from "./RoomItem";
+import './Room.css';
 import socketIoClient from 'socket.io-client'
-// const ENDPOINT = 'http://localhost/3000'
-// const connection = socketIoClient(ENDPOINT)
+const ENDPOINT = 'localhost:8080'
+const connection = socketIoClient(ENDPOINT)
 
-export default function Rooms(props) {
-  return (
+export default function Rooms() {
+
+//MND
+  const constraints = { audio: true, video: { width: 400, height: 300 } };
+  navigator.mediaDevices.getUserMedia(constraints)
+  .then(function(mediaStream) {
+    const video = document.querySelector('video');
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+  .catch(function(err) { console.log(err.name + ": " + err.message); }); 
+
+return (
+  <div>
     <h1>Rooms Page</h1>
-  )
-  // the rest of code should work after we got the props from app.js 
-  // const Rooms = props.Rooms;
-  // const List = Rooms.map((room) => <RoomItem key={room.id} name={room.name} />);
-  // return (
-  //   <ul>{List}</ul>
-  // )
-}
+    <video></video>
+  </div>
+)
+// the rest of code should work after we got the props from app.js 
+// const Rooms = props.Rooms;
+// const List = Rooms.map((room) => <RoomItem key={room.id} name={room.name} />);
+// return (
+//   <ul>{List}</ul>
+// )
+};
